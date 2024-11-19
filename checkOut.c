1ee082b72e826bacf75d0732485582f8313e1207
BHASVIC-TomParry24 <tom.parry24@students.bhasvic.ac.uk>
2024-11-19 16:09:20 +0000

    started checkOut

--- checkOut.c
+++ checkOut.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "project.h"
 
 void checkOut(){
-  char bookingID[30], newspaper;
-  float totalRoomCost, totalBoardCost;
-  int mainUserAge, numChildren;
+  int bookingIdFound = 0;
+  char bookingId[30];
+  char roomFileName[10];
+  int roomNumber;
+  char* currentBookingId;
+  int totalRoomCost;
+
   printf("Enter booking ID: ");
-  scanf("%c", bookingID);
+  scanf("%s", bookingId);
 
+  for(int i = 1; i <= 6; i++) {
+    sprintf(roomFileName, "room%d.txt", i);
+    currentBookingId = read(roomFileName, 10);
+    currentBookingId[strlen(currentBookingId) - 1] = '\0';
 
+    if (strcmp(currentBookingId, bookingId) == 0) {
+      roomNumber = i;
+      bookingIdFound = 1;
+      break;
+    };
+  }
+  if (bookingIdFound == 0) {
+    printf("Your booking ID could not be found in our system");
+  }
 
-
-
-
-
-
-
-
-
-
+  char* roomCostString = read(roomFileName, 11);
+  printf("%s\n", roomCostString);
+  sscanf(roomCostString, "%d", &totalRoomCost);
+  printf("%d\n", totalRoomCost);
 }
